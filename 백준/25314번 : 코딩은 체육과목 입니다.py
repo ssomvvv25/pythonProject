2c6ff3d28b439beba6bf548e540a92c4a903e104
소민 <eesomin@gmail.com>
2024-04-08 22:03:18 +0900

    0408커밋

---
+++ 백준/25314번 : 코딩은 체육과목 입니다.py
@@ -0,0 +1,26 @@
+"""
+#1
+N = int(input())
+n = int(N/4)
+a= 'long '*n
+print(a+'int')
+"""
+
+"""
+#2
+for i in range(int(input())//4):
+    print("long", end =" ")
+print("int")
+"""
+
+
+"""
+#3
+for _ in range(int(input())//4):
+    print("long", end =" ")
+print("int")
+"""
+
+#4
+print(int(input())//4*'long ' + 'int')
+
--- 백준/빠른 A+B.py
+++ 백준/빠른 A+B.py
@@ -1,1 +1,15 @@
-# 테스트케이스 입력받기
+import sys
+
+T = int(sys.stdin.readline())
+for i in range(T):
+    A,B = map(int, sys.stdin.readline().split())
+    print(A+B)
+
+
+"""
+#2
+T = int(input())
+for i in range(T):
+    a,b = map(int, sys.stdin.readline().split()) # 여기가 포인트
+    print(a+b)
+"""
