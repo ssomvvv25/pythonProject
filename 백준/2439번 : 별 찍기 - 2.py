f0e20596405880673f06a81e48219f78fd2bb615
소민 <eesomin@gmail.com>
2024-04-11 22:52:26 +0900

    0411커밋

--- 백준/2439번 : 별 찍기 - 2.py
+++ 백준/2439번 : 별 찍기 - 2.py
@@ -1,4 +1,14 @@
+
+#1
 N = int(input())
 
 for i in range(1,N+1):
-    print('*' * i)
+    a = N -i
+    print(' ' *a+'*'*i)
+
+
+#2
+N = int(input())
+
+for i in range(1,N+1):
+    print(' ' *(N-i)+'*'*i)
