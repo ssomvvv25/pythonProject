f7f65925e26e9fc461f7d21ed2211ad3ae2a374c
소민 <eesomin@gmail.com>
2024-04-14 23:40:44 +0900

    0414커밋3

--- 백준/1차원 배열/10871번 : X보다 작은 수.py
+++ 백준/1차원 배열/10871번 : X보다 작은 수.py
@@ -1,9 +1,5 @@
-import numpy as np
-
 N,X = map(int,input().split())
 N_list = list(map(int,input().split()))
-X_list = []
-for i in N_list:
-    if i < X:
-        np.append(X_list,i)
-    print(X_list)
+for i in range(N):
+    if N_list[i] < X:
+        print(N_list[i], end = " ")
